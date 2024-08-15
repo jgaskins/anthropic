12d14a884741a9263510ab0de2b57e5cad3958a8
Jamie Gaskins <jgaskins@hey.com>
2024-08-14 20:10:49 -0500

    Remove commented-out code
    This was never necessary. I copy/pasted it from another shard.

--- src/resource.cr
+++ src/resource.cr
@@ -5,17 +5,5 @@ module Anthropic
     macro included
       include JSON::Serializable
     end
-
-    # macro field(var, key = nil, &block)
-    #   @[JSON::Field(key: {{key ? key : var.var.stringify}})]
-    #   # @[MessagePack::Field(key: {{key ? key : var.var.camelcase(lower: true).stringify}})]
-    #   getter {{var}} {{block}}
-    # end
-
-    # macro field?(var, key = nil, &block)
-    #   @[JSON::Field(key: {{key ? key : var.var.stringify}})]
-    #   # @[MessagePack::Field(key: {{key ? key : var.var.camelcase(lower: true).stringify}})]
-    #   getter? {{var}} {{block}}
-    # end
   end
 end
