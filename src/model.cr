3c36ea21819706d96b237c3f61a4d704c2d13f0f
Jamie Gaskins <jgaskins@hey.com>
2024-10-30 21:16:23 -0500

    Use "-latest" alias
    This way we don't have to update the shard each time Anthropic updates a
    model.

--- src/model.cr
+++ src/model.cr
@@ -1,8 +1,7 @@
 module Anthropic
   private MODELS = {
     Model::Haiku  => "claude-3-haiku-20240307",
-    Model::Sonnet => "claude-3-sonnet-20240229",
-    Model::Sonnet => "claude-3-5-sonnet-20240620",
+    Model::Sonnet => "claude-3-5-sonnet-latest",
     Model::Opus   => "claude-3-opus-20240229",
   }
 
