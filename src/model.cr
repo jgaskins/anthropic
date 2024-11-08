404c3f884473eb56a06c8b000dd498771f589670
Jamie Gaskins <jgaskins@hey.com>
2024-11-08 00:22:12 -0600

    Update Haiku reference to 3.5
    Claude 3 Haiku still exists as :haiku3

--- src/model.cr
+++ src/model.cr
@@ -1,6 +1,7 @@
 module Anthropic
   private MODELS = {
-    Model::Haiku  => "claude-3-haiku-20240307",
+    Model::Haiku3 => "claude-3-haiku-20240307",
+    Model::Haiku  => "claude-3-5-haiku-latest",
     Model::Sonnet => "claude-3-5-sonnet-latest",
     Model::Opus   => "claude-3-opus-20240229",
   }
