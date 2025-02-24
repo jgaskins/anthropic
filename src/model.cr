0682d5928e6098c515e075419c5fed93df5f2568
Jamie Gaskins <jgaskins@hey.com>
2025-02-24 17:25:47 -0600

    Sonnet is 3.7 Sonnet now
    Reasoning coming soon

--- src/model.cr
+++ src/model.cr
@@ -2,7 +2,7 @@ module Anthropic
   private MODELS = {
     Model::Haiku3 => "claude-3-haiku-20240307",
     Model::Haiku  => "claude-3-5-haiku-latest",
-    Model::Sonnet => "claude-3-5-sonnet-latest",
+    Model::Sonnet => "claude-3-7-sonnet-20250219",
     Model::Opus   => "claude-3-opus-20240229",
   }
 
