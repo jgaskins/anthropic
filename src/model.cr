3518c8aa8c2436ad1c46cc038e6c40e2123cd0fa
Jamie Gaskins <jgaskins@hey.com>
2025-05-31 11:12:53 -0500

    Update model list

--- src/model.cr
+++ src/model.cr
@@ -1,9 +1,12 @@
 module Anthropic
   private MODELS = {
-    Model::Haiku3 => "claude-3-haiku-20240307",
-    Model::Haiku  => "claude-3-5-haiku-latest",
-    Model::Sonnet => "claude-3-7-sonnet-20250219",
-    Model::Opus   => "claude-3-opus-20240229",
+    Model::Haiku     => "claude-3-5-haiku-latest",
+    Model::Haiku3    => "claude-3-haiku-20240307",
+    Model::Sonnet    => "claude-sonnet-4-20250514",
+    Model::Sonnet3_5 => "claude-3-5-sonnet-20241022",
+    Model::Sonnet3_7 => "claude-3-7-sonnet-20250219",
+    Model::Opus      => "claude-opus-4-20250514",
+    Model::Opus3     => "claude-3-opus-20240229",
   }
 
   def self.model_name(model : Model)
@@ -14,6 +17,9 @@ module Anthropic
     Haiku
     Haiku3
     Sonnet
+    Sonnet3_5
+    Sonnet3_7
     Opus
+    Opus3
   end
 end
