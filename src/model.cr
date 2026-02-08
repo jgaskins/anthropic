3be22cb272641e5d3d5d5f41efda949a020368bd
Jamie Gaskins <jgaskins@hey.com>
2026-02-08 13:56:25 -0500

    Update model references

--- src/model.cr
+++ src/model.cr
@@ -1,12 +1,8 @@
 module Anthropic
   private MODELS = {
-    Model::Haiku     => "claude-3-5-haiku-latest",
-    Model::Haiku3    => "claude-3-haiku-20240307",
-    Model::Sonnet    => "claude-sonnet-4-20250514",
-    Model::Sonnet3_5 => "claude-3-5-sonnet-20241022",
-    Model::Sonnet3_7 => "claude-3-7-sonnet-20250219",
-    Model::Opus      => "claude-opus-4-20250514",
-    Model::Opus3     => "claude-3-opus-20240229",
+    Model::Haiku     => "claude-haiku-4-5",
+    Model::Sonnet    => "claude-sonnet-4-5",
+    Model::Opus      => "claude-opus-4-6",
   }
 
   def self.model_name(model : Model)
@@ -15,11 +11,7 @@ module Anthropic
 
   enum Model
     Haiku
-    Haiku3
     Sonnet
-    Sonnet3_5
-    Sonnet3_7
     Opus
-    Opus3
   end
 end
